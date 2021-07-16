567080744742bc39d50bc1e5d7fe3512760bbcd3
Marcelo Schneider <schneider.fei@gmail.com>
2021-07-16 19:10:42 -0300

    Cria formulário para local #1
    Cria formulário para adicionar novo local

---
+++ resources/views/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <script src="{{ asset('js/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <title>Locais - @yield('title')</title>
+</head>
+<body>
+    <nav class="navbar navbar-light bg-light sticky-top shadow-sm mb-3">
+        <div class="container-fluid">
+            <a href="" class="navbar-brand text-primary">
+               <strong>LOCAIS</strong>
+            </a>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        @yield('content')
+    </div>
+</body>
+</html>
