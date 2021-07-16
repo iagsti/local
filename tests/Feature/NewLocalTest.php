547a801bf1f448ea2e57ae51c0900275c241e6ed
Marcelo Schneider <schneider.fei@gmail.com>
2021-07-16 11:00:28 -0300

    Testa Novo Local
    Testa a criação de novo local

--- tests/Feature/NewLocalTest.php
+++ tests/Feature/NewLocalTest.php
@@ -2,21 +2,37 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+
 
 class NewLocalTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Test status code. It shoud be 200.
      *
      * @return void
      */
-    public function test_status_code_is_200()
+    public function test_status_code()
     {
-        $response = $this->get(route('new-local'));
-
+        $response = $this->get(route('local.create'));
         $response->assertStatus(200);
+    }
+
+    public function test_has_title()
+    {
+        $response = $this->get(route('local.create'));
+        $response->assertSee('Adicionar Novo Local');
+    }
+
+    public function test_see_form_fields()
+    {
+
+        $response = $this->get(route('local.create'));
+
+        $response->assertSee('bloco');
+        $response->assertSee('nome');
+        $response->assertSee('pavimento');
+        $response->assertSee('departamento');
+        $response->assertSee('descricao');
     }
 }
