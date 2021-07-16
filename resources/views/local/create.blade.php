bdd6f009f84f683c729289e909d83bf77e726855
Marcelo Schneider <schneider.fei@gmail.com>
2021-07-16 11:15:30 -0300

    Adiciona labels ao formulário

--- resources/views/local/create.blade.php
+++ resources/views/local/create.blade.php
@@ -1,9 +1,19 @@
 <h1>Adicionar Novo Local</h1>
 
 <form action="#" method="post">
-    <input type="text" name="nome" id="local-nome">
-    <input type="text" name="bloco" id="local-bloco">
-    <input type="text" name="pavimento" id="local-pavimento">
-    <input type="text" name="departamento" id="local-departamento">
-    <input type="text" name="descricao" id="local-descricao">
+    <label for="local-nome" class="form-label">Nome:</label>
+    <input type="text" class="form-control" name="nome" id="local-nome" aria-describedby="NomeLocal">
+
+    <label for="local-bloco" class="form-control">Bloco:</label>
+    <input type="text" class="form-control" name="bloco" id="local-bloco">
+
+    <label for="local-pavimento">Pavimento:</label>
+    <input type="text" class="form-control" name="pavimento" id="local-pavimento">
+
+    <label for="local-departamento">Departamento:</label>
+    <input type="text" class="form-control" name="departamento" id="local-departamento">
+
+    <label for="local-descricao">Descricao:</label>
+    <textarea name="descricao" id="local-descricao" cols="30" rows="10"></textarea>
+
 </form>
