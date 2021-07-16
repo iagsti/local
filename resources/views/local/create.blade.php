116440782dfc9a02c6e75cee63ff036fd4ca05a9
Marcelo Schneider <schneider.fei@gmail.com>
2021-07-16 10:54:53 -0300

    Cria view para novo local
    Cria view para adicionar no local

---
+++ resources/views/local/create.blade.php
@@ -0,0 +1,9 @@
+<h1>Adicionar Novo Local</h1>
+
+<form action="#" method="post">
+    <input type="text" name="nome" id="local-nome">
+    <input type="text" name="bloco" id="local-bloco">
+    <input type="text" name="pavimento" id="local-pavimento">
+    <input type="text" name="departamento" id="local-departamento">
+    <input type="text" name="descricao" id="local-descricao">
+</form>
