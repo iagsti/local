ae5c90e41a29afb9b1b3aee72dea73e3f794d20b
Marcelo Schneider <schneider.fei@gmail.com>
2021-07-17 20:33:51 -0300

    Testa requisições post para controller local #1
    Testa as requisições post para criação de novo local

--- tests/Feature/NewLocalTest.php
+++ tests/Feature/NewLocalTest.php
@@ -3,9 +3,11 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\Local;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use League\CommonMark\Block\Element\ThematicBreak;
 
-
-class NewLocalTest extends TestCase
+class NewLocalGetTest extends TestCase
 {
 
     private $response;
@@ -51,5 +53,64 @@ class NewLocalTest extends TestCase
         $this->response->assertSee('pavimento');
         $this->response->assertSee('departamento');
         $this->response->assertSee('descricao');
+    }
+}
+
+
+class NewLocalPostTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    /**
+     * Response Object
+     */
+    private $response;
+
+    /**
+     * Local data
+     */
+    private $data;
+
+    /**
+     * Test initialization
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->data = Local::factory()->make()->toArray();
+        $this->response = $this->followingRedirects()->post(route('local.store'), $this->data);
+    }
+
+    /**
+     * Test status code is 302
+     *
+     * @return void
+     */
+    public function test_status_code_is_200(): void
+    {
+        $this->response->assertStatus(200);
+    }
+
+    /**
+     * Test if local object was persited
+     *
+     * @return void
+     */
+    public function test_local_was_created(): void
+    {
+        $actual = $this->data['nome'];
+        $expected = Local::all()->first()->nome;
+        $this->assertEquals($actual, $expected);
+    }
+
+    /**
+     * Test if successful message was shown
+     */
+    public function test_see_successful_flash_message(): void
+    {
+        $this->response->assertSee('O local foi criado com sucesso!');
     }
 }
