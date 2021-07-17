76631b280722b37f55ffcf54ae8a7f612f405589
Marcelo Schneider <schneider.fei@gmail.com>
2021-07-17 20:31:35 -0300

    Adiciona propriedade fillable em Local #1
    Adiciona a propriedade fillable com os campos que podem ser
    recebidos em Local

--- app/Models/Local.php
+++ app/Models/Local.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Local extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'bloco',
+        'pavimento',
+        'departamento',
+        'descricao',
+    ];
 }
