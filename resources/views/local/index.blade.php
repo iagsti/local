0f6a367eaded20edf15e472eb30f90f6dd43e970
Marcelo Schneider <schneider.fei@gmail.com>
2021-07-18 00:51:08 -0300

    Cria view index para local #3
    Cria a view index para lista de locais

---
+++ resources/views/local/index.blade.php
@@ -0,0 +1,47 @@
+@extends('main')
+
+@section('title')
+    Lista
+@endsection
+
+@section('content')
+    <div class="border-4 border-top border-primary shadow-sm">
+        <div class="card border-light">
+            <div class="card-header text-primary h4">
+                Lista de locais
+            </div>
+            <div class="card-body">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Bloco</th>
+                            <th scope="col">Pavimento</th>
+                            <th scope="col">Departamento</th>
+                            <th scope="col">Editar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($locals as $row)
+                            <tr>
+                                <td>{{ $row->nome }}</td>
+                                <td>{{ $row->bloco }}</td>
+                                <td>{{ $row->pavimento }}</td>
+                                <td>{{ $row->departamento }}</td>
+                                <td>
+                                    <a href="#" class="btn btn-outline-primary" role="button">
+                                        <i class="bi bi-pencil-square"></i>
+                                        Editar
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="mt-3">
+                    {{ $locals->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
