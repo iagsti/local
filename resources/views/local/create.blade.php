7eff7f1aeedc9a484ce24f2bb613e1778d0c70a7
Marcelo Schneider <schneider.fei@gmail.com>
2021-07-22 21:52:55 -0300

    Substitui as opções hardcoded #5
    Substitui as opções hardcoded pelas opções informadas na
    configuração

--- resources/views/local/create.blade.php
+++ resources/views/local/create.blade.php
@@ -27,38 +27,28 @@
                     <div class="mb-3">
                         <label for="local-bloco" class="form-label fw-bold text-primary">Bloco:</label>
                         <select name="bloco" class="form-select" id="local-bloco">
-                            <option value="0">Não se aplica</option>
-                            <option value="A">A</option>
-                            <option value="B">B</option>
-                            <option value="C">C</option>
-                            <option value="D">D</option>
-                            <option value="E">E</option>
-                            <option value="F">F</option>
-                            <option value="G">G</option>
-                            <option value="PRINCIPAL">PRINCIPAL</option>
+                            @foreach ($blocos as $option)
+                                <option value="{{ $option['value'] }}">{{ $option['name'] }}</option>
+                            @endforeach
                         </select>
                     </div>
 
                     <div class="mb-3">
                         <label for="local-pavimento" class="form-label fw-bold text-primary">Pavimento:</label>
                         <select name="pavimento" class="form-select" id="local-pavimento">
-                            <option value="0">Não se aplica</option>
-                            <option value="terreo">Primeiro</option>
-                            <option value="primeiro">Segundo</option>
-                            <option value="segundo">Terceiro</option>
-                            <option value="terceiro">Quarto</option>
+                            @foreach ($pavimentos as $option)
+                                <option value="{{ $option['value'] }}">{{ $option['name'] }}</option>
+                            @endforeach
                         </select>
                     </div>
 
                     <div class="mb-3">
                         <label for="local-departamento" class="form-label fw-bold text-primary">Departamento:</label>
                         <select name="departamento" class="form-select" id="local-departamento">
-                            <option value="0">Não se aplica</option>
-                            <option value="astronomia">Astronomia</option>
-                            <option value="atmosfericas">Ciências Atmosféricas</option>
-                            <option value="geofisica">Geofísica</option>
-                            <option value="adm">Administração</option>
-                            <option value="biblioteca">Biblioteca</option>
+                            @foreach ($departamentos as $option)
+                                <option value="{{ $option['value'] }}">{{ $option['name'] }}</option>
+                            @endforeach
+
                         </select>
                     </div>
 
