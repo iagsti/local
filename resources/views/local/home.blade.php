7a2bc98c8a1674a66ea60b259adf30eb215c6b71
Marcelo Schneider <schneider.fei@gmail.com>
2021-07-23 17:49:57 -0300

    Cria template home #8

---
+++ resources/views/local/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('header')
+</head>
+
+<html>
+
+<body class="bg-front">
+    @include('menu')
+    <div class="container h-75 d-flex align-items-center">
+        <div class="row">
+            <div class="col-md-8">
+                <h1 class="display-4 text-white">
+                    Gerenciamento das dependências do IAG
+                </h1>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+</html>
