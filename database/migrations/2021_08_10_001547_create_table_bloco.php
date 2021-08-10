90cfcd8a4bcb1104a19035b5093168372d8ca64b
Marcelo Schneider <schneider.fei@gmail.com>
2021-08-09 22:45:00 -0300

    Modifica a coluna nome para uniique #16

--- database/migrations/2021_08_10_001547_create_table_bloco.php
+++ database/migrations/2021_08_10_001547_create_table_bloco.php
@@ -16,7 +16,7 @@ class CreateTableBloco extends Migration
         Schema::create('blocos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('nome', 10);
+            $table->string('nome', 10)->unique();
         });
     }
 
